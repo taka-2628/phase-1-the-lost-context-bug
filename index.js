c13b28cb1c40e1684035943a38d4bc8b69be59ce
Takashi <th2826@columbia.edu>
2021-09-18 14:24:39 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const messageConfig = {
   frontContent: "Happy Birthday, Odin One-Eye!",
   insideContent: "From Asgard to Nifelheim, you're the best all-father ever.\n\nLove,",
   closing: {
-      "Thor": "Admiration, respect, and love",
-      "Loki": "Your son"
+      Thor: "Admiration, respect, and love",
+      Loki: "Your son"
   },
   signatories: [
       "Thor",
